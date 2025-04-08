ba4db68190a1ddd572fb9b187e58ff3239ce431b
Mayur Rastogi <mayurrastogi46@gmail.com>
2025-04-08 19:24:41 +0530

    update comp

--- src/Pages/Home/Components/CallToAction.js
+++ src/Pages/Home/Components/CallToAction.js
@@ -1,11 +1,13 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
 
 const CallToAction = () => {
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <section className="call-to-action">
+      <h2>Hurry up Book your Tickets</h2>
+      <Link to="/pricing">Know More!</Link>
+    </section>
+  );
+};
 
-export default CallToAction
+export default CallToAction;
